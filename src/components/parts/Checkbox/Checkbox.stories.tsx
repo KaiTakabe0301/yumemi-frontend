19244b35aa710ad5ebffe6e344a59876d004a00d
toefuVox <ph.c11.java.eo12@gmail.com>
2024-09-02 03:45:04 +0900

    fix: 🐛 Checkboxのstorybookを修正

--- src/components/parts/Checkbox/Checkbox.stories.tsx
+++ src/components/parts/Checkbox/Checkbox.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react';
 import { useState } from 'react';
 
 import { Checkbox } from './Checkbox';
@@ -12,8 +12,32 @@ const meta = {
   tags: ['autodocs'],
 } satisfies Meta<typeof Checkbox>;
 export default meta;
+type Story = StoryObj<typeof meta>;
 
-export const Default = () => {
+export const Default: Story = {
+  argTypes: {
+    checked: {
+      control: {
+        type: 'boolean',
+      },
+    },
+    onCheckedChange: {
+      action: 'onCheckedChange',
+    },
+    label: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
+  args: {
+    label: 'label',
+    checked: false,
+    onCheckedChange: () => undefined,
+  },
+};
+
+export const Controlled = () => {
   const [checked, setChecked] = useState<boolean>(false);
 
   return (
