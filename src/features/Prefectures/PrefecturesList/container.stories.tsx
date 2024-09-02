8f1979da66b892fba8a5d1fd4778ff4a82dd18cc
toefuVox <ph.c11.java.eo12@gmail.com>
2024-09-02 23:26:05 +0900

    test: 💍 PrefecturesListのcomponent testを実装

--- src/features/Prefectures/PrefecturesList/container.stories.tsx
+++ src/features/Prefectures/PrefecturesList/container.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { expect, waitFor, within } from '@storybook/test';
 
 import { PrefectureList } from './container';
 
@@ -14,5 +15,40 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: {},
+  tags: ['test-only'],
+  play: async ({ canvasElement, step }) => {
+    const canvas = within(canvasElement);
+
+    await step('Check Initial Visibility of CheckBoxes', async () => {
+      await waitFor(
+        () => {
+          expect(canvas.getByLabelText('北海道')).toBeVisible();
+          expect(canvas.getByLabelText('青森県')).toBeVisible();
+        },
+        {
+          timeout: 5000,
+        },
+      );
+    });
+
+    await step('Select CheckBox', async () => {
+      await canvas.getByLabelText('北海道').click();
+      await canvas.getByLabelText('青森県').click();
+    });
+
+    await step('Verify CheckBoxes Are Selected', async () => {
+      await expect(canvas.getByLabelText('北海道')).toBeChecked();
+      await expect(canvas.getByLabelText('青森県')).toBeChecked();
+    });
+
+    await step('Deselect CheckBoxes', async () => {
+      await canvas.getByLabelText('北海道').click();
+      await canvas.getByLabelText('青森県').click();
+    });
+
+    await step('Verify CheckBoxes Are Deselected', async () => {
+      await expect(canvas.getByLabelText('北海道')).not.toBeChecked();
+      await expect(canvas.getByLabelText('青森県')).not.toBeChecked();
+    });
+  },
 };
