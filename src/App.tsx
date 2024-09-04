2be6737e60437139449a6c80a9abd59bee1dcddb
toefuVox <ph.c11.java.eo12@gmail.com>
2024-09-05 00:16:50 +0900

    refactor: 💡 QueryClientProviderの位置を修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Suspense } from 'react';
 
 import { Skeleton } from './components/parts/Skeleton';
 import { Population } from './features/Population';
 import { PrefectureList } from './features/Prefectures/PrefecturesList';
-// Create a client
-const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-      <QueryClientProvider client={queryClient}>
-        <PrefectureList />
-        <Suspense fallback={<Skeleton className='w-[90%] h-[420px] bg-slate-200' />}>
-          <Population />
-        </Suspense>
-      </QueryClientProvider>
+      <PrefectureList />
+      <Suspense fallback={<Skeleton className='w-[90%] h-[420px] bg-slate-200' />}>
+        <Population />
+      </Suspense>
     </>
   );
 }
