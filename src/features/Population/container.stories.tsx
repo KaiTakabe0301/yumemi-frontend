a026b13344b3b5ca1bb4df05b42253385b5d6174
toefuVox <ph.c11.java.eo12@gmail.com>
2024-09-04 17:58:32 +0900

    test: 💍 グラフ描画のcomponent testを追加

--- src/features/Population/container.stories.tsx
+++ src/features/Population/container.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { expect, userEvent, waitFor, within } from '@storybook/test';
 import { reactRouterParameters } from 'storybook-addon-remix-react-router';
 
 import { mockPrefectures } from '../../mocks/handlers/api/resas/prefectures';
@@ -33,5 +34,84 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     prefectures: mockPrefectures,
+  },
+  parameters: {
+    viewport: {
+      defaultViewport: 'tablet',
+    },
+  },
+  tags: ['testable'],
+  play: async ({ canvasElement, step }) => {
+    const canvas = within(canvasElement);
+
+    // svgの描画が完了するまで待つ
+    await waitFor(
+      () => {
+        expect(
+          canvasElement.querySelector('svg')?.querySelector('path[name=北海道]'),
+        ).toBeVisible();
+      },
+      { timeout: 5000 },
+    );
+
+    // 総人口のsvg pathを取得
+    const svgElement = await canvasElement.querySelector('svg');
+    let prevSvgPath = svgElement?.querySelector('path[name=北海道]');
+
+    // Comboboxを取得
+    const comboboxInput = canvas.getByRole('combobox');
+
+    await step('Select option from Combobox', async () => {
+      await userEvent.selectOptions(comboboxInput, '年少人口');
+    });
+
+    // SVGが変更されるのを待ってから確認
+    await step('Check if SVG is updated for 年少人口', async () => {
+      const svgElement = await canvasElement.querySelector('svg');
+      const svgPath = svgElement?.querySelector('path[name=北海道]');
+      await waitFor(
+        async () => {
+          await expect(svgPath).not.toHaveAttribute('d', prevSvgPath?.getAttribute('d'));
+        },
+        { timeout: 5000 },
+      );
+
+      prevSvgPath = svgPath;
+    });
+
+    // オプションを選択
+    await step('Select option from Combobox', async () => {
+      await userEvent.selectOptions(comboboxInput, '生産年齢人口');
+    });
+
+    // SVGが変更されるのを待ってから確認
+    await step('Check if SVG is updated for 生産年齢人口', async () => {
+      const svgElement = await canvasElement.querySelector('svg');
+      const svgPath = svgElement?.querySelector('path[name=北海道]');
+      await waitFor(
+        async () => {
+          await expect(svgPath).not.toHaveAttribute('d', prevSvgPath?.getAttribute('d'));
+        },
+        { timeout: 5000 },
+      );
+
+      prevSvgPath = svgPath;
+    });
+
+    await step('Select option from Combobox', async () => {
+      await userEvent.selectOptions(comboboxInput, '老年人口');
+    });
+
+    // // SVGが変更されるのを待ってから確認
+    await step('Check if SVG is updated for 老年人口', async () => {
+      const svgElement = await canvasElement.querySelector('svg');
+      const svgPath = svgElement?.querySelector('path[name=北海道]');
+      await waitFor(
+        async () => {
+          await expect(svgPath).not.toHaveAttribute('d', prevSvgPath?.getAttribute('d'));
+        },
+        { timeout: 5000 },
+      );
+    });
   },
 };
