7e5f5bb739173121a937689ea4a7f06438b110b1
toefuVox <ph.c11.java.eo12@gmail.com>
2024-09-04 23:22:38 +0900

    refactor: 💡 useSWR->useQuery

--- src/features/Prefectures/useFetchPrefectures.tsx
+++ src/features/Prefectures/useFetchPrefectures.tsx
@@ -1,13 +1,13 @@
-import useSWR from 'swr';
+import { useQuery } from '@tanstack/react-query';
 
 import { fetcher } from '../../utils/fetcher';
 import { Prefecture } from './type';
 
 export function useFetchPrefectures() {
-  const { data, error } = useSWR<{ message: null; result: Prefecture[] }>(
-    '/api/resas/prefectures',
-    fetcher,
-  );
+  const { data, error } = useQuery<{ message: null; result: Prefecture[] }>({
+    queryKey: ['/api/resas/prefectures'],
+    queryFn: () => fetcher('/api/resas/prefectures'),
+  });
 
   return { data: data?.result, error };
 }
