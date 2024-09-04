52cb22398239f75c2aba20f5e22a6c59a67b6969
toefuVox <ph.c11.java.eo12@gmail.com>
2024-09-04 19:03:28 +0900

    test: 💍 アプリのcomponent testを実装

--- src/App.stories.tsx
+++ src/App.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { expect, waitFor, within } from '@storybook/test';
 
 import App from './App';
 
@@ -14,4 +15,69 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {},
+  tags: ['testable'],
+  play: async ({ canvasElement, step }) => {
+    const canvas = within(canvasElement);
+
+    // 都道府県の選択肢が表示されていることを確認
+    await waitFor(
+      () => {
+        expect(canvas.getByLabelText('北海道')).toBeVisible();
+      },
+      {
+        timeout: 5000,
+      },
+    );
+
+    await step('北海道を選択', async () => {
+      await canvas.getByLabelText('北海道').click();
+    });
+
+    await step('グラフ上に、北海道の総人口が描画されていることを確認', async () => {
+      await waitFor(
+        async () => {
+          expect(
+            canvasElement.querySelector('svg.recharts-surface')?.querySelector('path[name=北海道]'),
+          ).toBeVisible();
+        },
+        { timeout: 5000 },
+      );
+    });
+
+    await step('追加で東京都を選択', async () => {
+      await canvas.getByLabelText('東京都').click();
+    });
+
+    await step('グラフ上に、北海道と東京都のグラフが描画されていることを確認', async () => {
+      await waitFor(
+        async () => {
+          expect(
+            canvasElement.querySelector('svg.recharts-surface')?.querySelector('path[name=北海道]'),
+          ).toBeVisible();
+          expect(
+            canvasElement.querySelector('svg.recharts-surface')?.querySelector('path[name=東京都]'),
+          ).toBeVisible();
+        },
+        { timeout: 5000 },
+      );
+    });
+
+    await step('北海道の選択を解除', async () => {
+      await canvas.getByLabelText('北海道').click();
+    });
+
+    await step('グラフ上に東京都のみが描画されていることを確認', async () => {
+      await waitFor(
+        async () => {
+          expect(
+            canvasElement.querySelector('svg.recharts-surface')?.querySelector('path[name=北海道]'),
+          ).toBeNull();
+          expect(
+            canvasElement.querySelector('svg.recharts-surface')?.querySelector('path[name=東京都]'),
+          ).toBeVisible();
+        },
+        { timeout: 5000 },
+      );
+    });
+  },
 };
